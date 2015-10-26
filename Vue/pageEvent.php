cd08adc7860c9e8a804c910cb890c01da4f6554d
nicolas <nicolas@172.16.236.213>
2015-10-26 11:50:36 +0100

    profil + event

---
+++ Vue/pageEvent.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>page événement</title>
+        <link href="./css/style.css" rel="stylesheet" media="all" type="text/css"> 
+        
+    </head>
+    <body>
+        <?php require("/header.php"); ?>
+        
+        <div id="content">
+            <div id="profilEvent">
+                <img id="photoProfil" src="./img/default-event.png" />
+                <div id="infosProfil">
+                    <p>Nom événement</p>
+                    <p>Lieu : </p>
+                    <p>Description : </p>
+                </div>
+            </div>
+            <div id="infosEvent">
+                <div id="infosOrganisateur">
+                    <p>Organisateur : </p>
+                    <p>Nombres d'événements organisés : </p>
+                    <a href=''>profil</a>
+                </div>
+                <div id="participationEvent">
+                    <button>Participer</button>
+                    <button>Ajouter à ses interet</button>
+                </div>
+            </div>
+            <div id="clear"></div>
+            <br>
+            <div id="infosComplementaires">
+                <div id="infosLieu" class="cadre">
+                    <div id="map"></div>
+                    <p>Lieu : </p>
+                </div>
+                <div id ="descriptionEvent" class="cadre">
+                    <h1>Description</h1>
+                    <p>
+                        ----------------------------------------------------------------------------------------------------------------
+                        ----------------------------------------------------------------------------------------------------------------
+                        ----------------------------------------------------------------------------------------------------------------
+                        ----------------------------------------------------------------------------------------------------------------<br>
+                        ----------------------------------------------------------------------------------------------------------------
+                        ----------------------------------------------------------------------------------------------------------------<br>
+                    </p>
+                        
+                </div>
+                
+                <div id="billeterie" class="cadre">
+                    <h1>Lien vers la billeterie</h1>
+                    <p>
+                        -------------------------------------------------
+                    </p>
+                </div>
+               
+                <div id="commentairesEvent" class="cadre">
+                    <h1>Commentaires</h1>
+                    <p>
+                    <?php
+                    for ($i = 0 ; $i<5 ; $i++){
+                        ?>
+                    <p class="cadre">
+                        Commentaire---------------------------------------------
+                        --------------------------------------------------------
+                        <br>
+                        --------------------------------------------------------
+                        --------------------------------------------------------
+                        --------------------------------------------------------
+                        <br>
+                    <a href=""> lire la suite</a>
+                    </p>
+                    <br>
+                    <?php
+                    }
+                    ?>
+                    </p>
+                </div>
+                <div id="clear"></div>
+            </div>
+        </div>    
+        
+        <div id="footer">
+            
+        </div>
+    </body>
+</html>
--- Vue/profilUtilisateur.php
+++ Vue/profilUtilisateur.php
@@ -15,14 +15,48 @@ and open the template in the editor.
         <?php require("/header.php"); ?>
         
         <div id="content">
-            <div id="profil"></div>
-                Profil
-            <div>
+            <div id="profil">
+                <img id="photoProfil" src="./img/default-user.png" />
+                <div id="infosProfil">
+                    <p>Prenom NOM</p>
+                    <p>Ville</p>
+                    <p>Evenements inscrits : </p>
+                    <p>Evenements organisés : </p>
+                </div>
+                <div id="clear"></div>
+            </div>
+            <br>
+            <div id="infosComplementaires">
                 <div id="agenda">
-                    Agenda
+                    <p class="titre">Agenda</p>
+                    
+                    <?php
+                    for ($i = 0 ; $i<5 ; $i++){
+                        ?>
+                    <a class="eventProfil" href=""> lien événement</a>
+                    <br>
+                    <?php
+                    }
+                    ?>
                 </div>
                 <div id="commentaires">
-                    Commentaires
+                    <p class="titre">Commentaires</p>
+                    <p>
+                    <?php
+                    for ($i = 0 ; $i<5 ; $i++){
+                        ?>
+                    <p class="comsProfil">
+                        Commentaire---------------------------------------------
+                        <br>
+                        --------------------------------------------------------
+                        <br>
+                    <a href=""> lire la suite</a>
+                    </p>
+                    <br>
+                    <?php
+                    }
+                    ?>
+                    </p>
                 </div>
                 <div id="clear"></div>
             </div>
