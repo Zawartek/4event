07aabf2b3b5c02ec85312983427cc0c50860565c
jvoyat <julien.voyat@gmail.com>
2015-12-07 22:59:28 +0100

    FC

--- Vue/accueil.php
+++ Vue/accueil.php
@@ -1,3 +1,33 @@
+<?php
+// Le champ "identifiant" ET le champ "password" sont remplis ?
+if (isset($_POST['login'])) {
+    // Neutralisation des éventuelles mauvaises intentions du visiteur.
+    $login = htmlspecialchars($_POST['login']);
+    $password = htmlspecialchars($_POST['password']);
+
+    if (($login != '') && ($password != '')) {
+        $password = md5($password);
+
+        $reponse = $bdd->query("SELECT * FROM joueur WHERE login='$login'");
+        while ($donnees = $reponse->fetch()) {
+            // Le champ "password" correspond à l'un de ces membres ?
+            if ($password == $donnees['password']) {
+                // Ouverture d'une session "membre"
+                $_SESSION['id_joueur'] = $donnees['id'];
+
+                // Redirection vers la page "village".
+                header('Location: ' . URL . '/index.php?page=village');
+                exit();
+            }
+        }
+
+        $message = 'L\'identifiant ou le mot de passe sont incorrects.';
+    } else {
+        $message = 'Certains champs sont vides.';
+    }
+}
+?>
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -16,7 +46,7 @@ and open the template in the editor.
         <script src="js/datepicker.js"></script>
 
         <script type="text/javascript">
-            $(function ()
+            $(function()
             {
                 $("#date").datepicker($.datepicker.regional["fr"]);
                 $("#date").datepicker('setDate', new Date());
@@ -25,18 +55,18 @@ and open the template in the editor.
     </head>
     <body>
         <div id="content">
-            <?php require("./header.php"); ?>
+<?php require("./header.php"); ?>
             <div id="slider">
-                <?php include('slider/slider.html'); ?>
+<?php include('slider/slider.html'); ?>
 
                 <form id="barreRecherche" method="post" action="#">
                     <select id="choixTheme" name="choixTheme" class="input">
-                        <?php
-                        for ($i = 0; $i < 5; $i++) {
-                            ?>
+            <?php
+            for ($i = 0; $i < 5; $i++) {
+                ?>
                             <option>Test</option>
-                        <?php }
-                        ?>
+<?php }
+?>
                     </select>
                     <input type="text" id="date" name="date" class="date" onload="this.value(Date());">
                     <input type="submit" id="valider" class="bold btn btn-orange" value="Rechercher">
@@ -44,9 +74,9 @@ and open the template in the editor.
             </div>
             <select id="trierPar" name="trierPar" class="select">
                 <option>Trier par</option>
-                <?php
-                for ($i = 0; $i < 5; $i++) {
-                    ?>
+<?php
+for ($i = 0; $i < 5; $i++) {
+    ?>
                     <option>
                         Test
                     </option>
@@ -79,7 +109,7 @@ and open the template in the editor.
                 <div style="float:left; background-image: url('../Vue/img/default-event.png'); background-size: 100px 100px; background-repeat: no-repeat; width : 20%; height : 100px;"> Nom de l'évenement</div>
                 <div id="clear"></div>
             </div>
-            <?php include('./footer.php'); ?>
+<?php include('./footer.php'); ?>
         </div>
     </body>
 </html>
