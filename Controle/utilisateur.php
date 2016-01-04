43e879d8cfcb55028bf4b35ee66f19c16085bcf2
psacarabany <p.sacarabany@hotmail.fr>
2016-01-04 10:09:21 +0100

    sécurisation faille XSS
    pour les données utilisateurs lors de l'inscription

--- Controle/utilisateur.php
+++ Controle/utilisateur.php
@@ -72,16 +72,16 @@ function inscription() {
     if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) && isset($_POST['voie']) &&
             isset($_POST['codepostal']) && isset($_POST['ville']) && isset($_POST['pays']) && isset($_POST['datenaissance']) &&
             isset($_POST['mdp']) && isset($_POST['sexe'])) {
-        $nom = $_POST['nom'];
-        $prenom = $_POST['prenom'];
-        $email = $_POST['email'];
-        $voie = $_POST['voie'];
-        $codepostal = $_POST['codepostal'];
-        $ville = $_POST['ville'];
-        $pays = $_POST['pays'];
-        $datenaissance = formattageDateBDD($_POST['datenaissance']);
-        $mdp = $_POST['mdp'];
-        $sexe = $_POST['sexe'];
+        $nom = htmlspecialchars($_POST['nom']);
+        $prenom = htmlspecialchars($_POST['prenom']);
+        $email = htmlspecialchars($_POST['email']);
+        $voie =  htmlspecialchars($_POST['voie']);
+        $codepostal = htmlspecialchars($_POST['codepostal']);
+        $ville = htmlspecialchars($_POST['ville']);
+        $pays = htmlspecialchars($_POST['pays']);
+        $datenaissance = htmlspecialchars(formattageDateBDD($_POST['datenaissance']));
+        $mdp = htmlspecialchars($_POST['mdp']);
+        $sexe = htmlspecialchars($_POST['sexe']);
 
         if (isset($_POST['newsletter'])) {
             $newsletter = $_POST['newsletter'];
