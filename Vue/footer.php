d9458a4587cd354caf7b6d352702c1524de8bf98
Zawartek <masterdu92@hotmail.fr>
2016-01-15 09:46:25 +0100

    footer

--- Vue/footer.php
+++ Vue/footer.php
@@ -1,78 +1,55 @@
-
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="./Vue/css/footerStyle.css">
         <meta charset="UTF-8">
-        <title></title>
     </head>
     <body>
-        
+
     </body>
     <footer id="mainFooter">
-	<div id="mainFooter-bounds">
-		<div class="hautFooter">
-			
-			<div class="DivCreerGroupe">
+        <div id="mainFooter-bounds">
+            <div class="hautFooter">
+                <div class="DivCreerGroupe">
+                    <a id="startFooterBtn" class="footerCG TextBtn" href="#">
+                        Créer un groupe 4event
+                    </a>
+                </div>
+            </div>
+            <section>
+                <ul class="BodyFooter">
+                    <li id="ElmtID" class="ElmtBFooter">
+                        <a class="ElmtBFinterne" href="#">
+                            FAQ
+                        </a>
+                    </li>
+                    <li id="ElmtID" class="ElmtBFooter">
+                        <a class="ElmtBFinterne" href="#">
+                            Qui sommes-nous ?
+                        </a>
+                    </li>
+                    <li id="ElmtID" class="ElmtBFooter">
+                        <a  class="ElmtBFinterne" href="#" target="_blank">
+                            Made by Web'Design 2015 - 2016
+                        </a>
+                    </li>
+                </ul>
 
-					<a id="startFooterBtn" class="footerCG TextBtn" href="#">
-						Créer un groupe 4event
-					</a>
-			</div>
-			<div id="footerAuthLink" class="footerDiv_C">
-				
-						<a class="footerConnexion TextBtn" href="#">
-							Connexion
-						</a>
-			</div>
-		</div>
-		<section>
-			<ul class="BodyFooter">
-				<li id="ElmtID" class="ElmtBFooter">
-					<a class="ElmtBFinterne" href="#">
-						Aide
-					</a>
-				</li>
-				<li id="ElmtID" class="ElmtBFooter">
-					<a class="ElmtBFinterne" href="#">
-						Qui sommes-nous ?
-					</a>
-				</li>
-				<li id="ElmtID" class="ElmtBFooter">
-					<a class="ElmtBFinterne" href="#">
-						Offres d'emploi
-					</a>
-				</li>
-				<li id="ElmtID" class="ElmtBFooter">
-					<a class="ElmtBFinterne" href="#" target="">Blog</a>
-				</li>
-				<li id="ElmtID" class="ElmtBFooter"><a  class="ElmtBFinterne" href="#" target="_blank">Made at ISEP 2015 - 2016</a></li>
+                <ul class="FFooter">
+                    <li id="ElmtID" class="ElmtBFooter">© 2016 4Event</li>
+                    <li id="ElmtID" class="ElmtBFooter"><a class="ElmtBFinterne" href="#">Conditions d'Utilisations Générales</a></li>
+                </ul>
+            </section>
 
-			</ul>
-			
-			<ul class="FFooter">
-				<li id="ElmtID" class="ElmtBFooter">© 2016 4Event</li>
-				<li id="ElmtID" class="ElmtBFooter"><a class="ElmtBFinterne" href="#">Confidentialité</a></li>
-				<li id="ElmtID" class="ElmtBFooter"><a class="ElmtBFinterne" href="#">Conditions d'utilisations</a></li>
-			</ul>
-		</section>
-		
-		<div id="footer" class="MainFollow">
-			<ul>
-				<li id="ElmtID" class="ElmtBFooter"><a  class="ElmtBFinterne" target="_blank">Follow us :  </a>
-					<a class="lien-reseau" href="#"><img class="logo-reseau" src="./Vue/img/facebook.png" alt="facebook"></a>
-					<a class="lien-reseau" href="#"><img class="logo-reseau" src="./Vue/img/twitter.png" alt="twitter"></a>
-					<a class="lien-reseau" href="#"><img class="logo-reseau" src="./Vue/img/googleplus.png" alt="google+"></a>
-				
-				</li>
-				
-			</ul>
-		</div>
-	</div>
-</footer>
+            <div id="footer" class="MainFollow">
+                <ul>
+                    <li id="ElmtID" class="ElmtBFooter"><a  class="ElmtBFinterne" target="_blank">Suivez nous :  </a>
+                        <a class="lien-reseau" href="#"><img class="logo-reseau" src="./Vue/img/facebook.png" alt="facebook"></a>
+                        <a class="lien-reseau" href="#"><img class="logo-reseau" src="./Vue/img/twitter.png" alt="twitter"></a>
+                        <a class="lien-reseau" href="#"><img class="logo-reseau" src="./Vue/img/googleplus.png" alt="google+"></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
 </html>
